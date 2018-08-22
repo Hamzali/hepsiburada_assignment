4d1648b6e269333f7fb1ef7e0cb8c57dd7f7f0aa
Hamza Ali <hamzali.tas.95@gmail.com>
2018-08-22 13:41:49 +0300

    doc generating and more readme

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # HepsiburadaAssignment
 
-**TODO: Add description**
+**Assignment project for Hepsiburda case study**
 
 ## Installation
 
 - run: ```elixir mix```
 - test: ```elixir mix test```
 - build ```elixir mix escript.build```
+- docs ```elixir mix docs```
 
 ## [Documentation](https://hexdocs.pm/hepsiburada_assignment)
 
--- mix.exs
+++ mix.exs
@@ -27,6 +27,8 @@ defmodule HepsiburadaAssignment.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:earmark, "~> 1.2", only: :dev},
+      {:ex_doc, "~> 0.19", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
